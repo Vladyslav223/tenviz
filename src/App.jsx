cf4238320fdc0a6867d3f8345d0776dbf3743bc5
vladyslav223 <vladyslav223@gmail.com>
2020-04-07 19:35:32 +0300

    react app files, gh-pages scripts, images for test, linter added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">      
+        <img src="/tenviz/img/list.png" className="App-logo" alt="logo" />        
+        <div className="div"></div>     
+    </div>
+  );  
+}
+
+export default App;
