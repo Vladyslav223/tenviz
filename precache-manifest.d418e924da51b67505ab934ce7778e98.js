5d721207ce22e2dd74e3a79a1c926bf03873c299
vladyslav223 <vladyslav223@gmail.com>
2020-04-07 19:36:59 +0300

    Updates

---
+++ precache-manifest.d418e924da51b67505ab934ce7778e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111b48c5d66495c773fd06e547d6c4ed",
+    "url": "/tenviz/index.html"
+  },
+  {
+    "revision": "53ec1b2b33eca65ebda4",
+    "url": "/tenviz/static/css/main.cb80c82f.chunk.css"
+  },
+  {
+    "revision": "8d6e56f0a2a300f7c0b6",
+    "url": "/tenviz/static/js/2.32b67d19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tenviz/static/js/2.32b67d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ec1b2b33eca65ebda4",
+    "url": "/tenviz/static/js/main.d64e2bf0.chunk.js"
+  },
+  {
+    "revision": "2d03cb339e83e74ef6b2",
+    "url": "/tenviz/static/js/runtime-main.b5c36f9b.js"
+  }
+]);
